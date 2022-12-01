d36bd39666ca386fa3e50380406de9b49fe608ba
paikwiki <paikwiki@gmail.com>
2022-12-01 21:13:27 +0900

    :pencil2: 오타 수정

--- src/day01/solve.ts
+++ src/day01/solve.ts
@@ -8,18 +8,18 @@ export const solve = () => {
     .toString()
     .split(NEW_LINE_CHARACTER + NEW_LINE_CHARACTER);
 
-  const elvCalories = elves.map((elf) =>
+  const elfCalories = elves.map((elf) =>
     elf
       .split(NEW_LINE_CHARACTER)
       .reduce((acc: number, curr: string) => acc + parseInt(curr), 0)
   );
-  const partOneAnswer = elvCalories.reduce(
+  const partOneAnswer = elfCalories.reduce(
     (acc, curr) => (curr > acc ? curr : acc),
     0
   );
   console.log(`part one: ${partOneAnswer}`);
 
-  const partTwoAnswer = [...elvCalories]
+  const partTwoAnswer = [...elfCalories]
     .sort((a: number, b: number) => b - a)
     .splice(0, 3)
     .reduce((acc, curr) => acc + curr, 0);
