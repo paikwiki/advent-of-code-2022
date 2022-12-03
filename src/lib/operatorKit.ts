4e98e65f2470779bce6e5dc62c7a623ed53c192b
paikwiki <paikwiki@gmail.com>
2022-12-03 17:55:26 +0900

    :art: 함수 타입추론 개선: notSame()

--- src/lib/operatorKit.ts
+++ src/lib/operatorKit.ts
@@ -5,7 +5,8 @@ export const divide: NumberOperator = (lhs, rhs) => lhs / rhs;
 export const multiply: NumberOperator = (lhs, rhs) => lhs * rhs;
 
 export const same = <T>(lhs: T, rhs: T) => lhs === rhs;
-export const notSame = <T>(lhs: T, rhs: T) => lhs !== rhs;
+export const notSame = <T, K extends T>(lhs: T, rhs: K): lhs is Exclude<T, K> =>
+  lhs !== rhs;
 export const notNull = <T>(item: T): item is Exclude<T, null> => item !== null;
 
 export const compare = <T>({
